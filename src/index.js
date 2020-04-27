cc9f5f99eb9fe57de83f10c8c8bfe67032e579bc
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-04-27 08:08:30 +0100

    Updated index.js file to simplifty react render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 
-ReactDOM.render(
+//ReactDOM.render(
+//	<h1>Hello World</h1>,
+//	document.getElementById('app')
+//	);
+
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
 	<h1>Hello World</h1>,
 	document.getElementById('app')
-	);
+)
