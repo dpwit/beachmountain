fc16d45c3875a10b9e9f8c2eb1537050f0e021cb
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-04-27 08:29:23 +0100

    Updated index.js file with variable and run build

--- src/index.js
+++ src/index.js
@@ -14,3 +14,11 @@ render(
 	<h1>Hello World</h1>,
 	document.getElementById('app')
 )
+
+const name = 'Darren';
+const element = <h1>Hello {name}</h1>;
+
+render(
+	element,
+	document.getElementById('helloName')
+	);
