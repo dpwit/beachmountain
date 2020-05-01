58d1a5423bb3c1fb70713d975bddb8c9aa51675e
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-05-01 20:14:13 +0100

    Update Button.js to try to fix bug

--- src/Button.js
+++ src/Button.js
@@ -1,25 +1,20 @@
 
 import React from 'react';
 
-const Button = (props) => {
-	//list multiple props here
-	const { type = '',
-	propTwo,
-	propThree,
-	propFour } = props
-
-	return (
-		<button type={type} propTwo={propTwo} propThree={propThree} propFour={propFour}>This is a button</button>
-		)
-}
-
 function resetProduct() {
   document.getElementById('productLogo').src='img/DPW_Skull_Logo_Mask.jpg';
 }
 
-const resetProductAction = (
-  <button onClick={resetProduct}>Reset to original</button>
-);
+const Button = (props) => {
+	//list multiple props here
+	const { type = '',
+	onClick,
+	propThree,
+	propFour } = props
 
+	return (
+		<button type={type} onClick={resetProduct} propThree={propThree} propFour={propFour}>Reset to original</button>
+		)
+}
 
 export default Button
