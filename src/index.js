86a80c2061c760e9bbf28e6accab3f93959e2038
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-05-05 19:47:52 +0100

    Removed unwanted code to clean index js file

--- src/index.js
+++ src/index.js
@@ -12,28 +12,6 @@ import { render } from 'react-dom';
 
 import Button from './Button';
 
-render(
-	<h1>Hello World</h1>,
-	document.getElementById('app')
-)
-
-const name = 'Darren';
-const element = <h1>Hello {name}</h1>;
-
-render(
-	element,
-	document.getElementById('helloName')
-	);
 const rootElement = document.getElementById('appButton')
 
 render(<Button />, rootElement)
-
-//render(
-//	<Button type="submit" />,
-//	document.getElementById('appButton')
-//	);
-
-//render(
-//	<Button onClick="{resetProduct}" />, 
-//	document.getElementById('resetProductBtn')
-//	);
