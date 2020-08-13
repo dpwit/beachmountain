ef5108d9775624117cd4eb98170bb2a45ed0e862
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-08-13 23:08:57 +0100

    Added new file src/Reset.js

---
+++ src/Reset.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import Button from './Button'
+
+const Reset = () => {
+
+function resetProduct() {
+  	document.getElementById('productLogo').src='img/DPW_Skull_Logo_Mask.jpg';
+
+	document.getElementById("eyesBlue").checked = false;
+	document.getElementById("eyesGreen").checked = false;
+	document.getElementById("eyesGrey").checked = false;
+		
+}
+
+return (
+	<Button onClick={resetProduct}>Reset to original</Button>
+	);
+}
+
+
+export default Reset
