50924cd93b83cd3b3996f50e95404157517037af
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-08-13 22:51:14 +0100

    Updated index js and added Reset js to srtip out function ran build

--- src/index.js
+++ src/index.js
@@ -10,17 +10,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Button from './Button';
+import Reset from './Reset';
 
-const rootElement = document.getElementById('appButton')
+render(
+    <Reset />,
+    document.getElementById('appButton')
+);
 
-function resetProduct() {
-  	document.getElementById('productLogo').src='img/DPW_Skull_Logo_Mask.jpg';
-
-	document.getElementById("eyesBlue").checked = false;
-	document.getElementById("eyesGreen").checked = false;
-	document.getElementById("eyesGrey").checked = false;
-		
-}
-
-render(<Button onClick={resetProduct}>Reset to original</Button>, rootElement)
