9d28f6819c31331499253a53e51c2d8ce48ddea1
Darren Whatford <darrenwhatford@Darrens-MacBook-Air.local>
2026-06-07 16:32:57 +0100

    added basic header and header js file

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = () => {
+
+return (
+        <div class="display-inline-flex">
+    <img src="dist/img/Beach-Mountain-logo-blue-image.png" alt="beach mountain" class="bm-logo display-inline-flex">
+        <h1 class="display-inline-flex margin-top-16">beach mountain</h1>
+    </img>
+</div>
+)
+}
+
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-
-//ReactDOM.render(
-//	<h1>Hello World</h1>,
-//	document.getElementById('app')
-//	);
-
 import React from 'react';
 import { render } from 'react-dom';
-
+import Header from './Header';
 import Reset from './Reset';
 
 render(
     <Reset />,
     document.getElementById('appButton')
 );
+    
+
+
 
