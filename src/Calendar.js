4e800306182023dae001492a01eaab94b7f89022
Darren Whatford <darrenwhatford@Darrens-MacBook-Air.local>
2026-06-15 22:38:44 +0100

    added FullCalender

---
+++ src/Calendar.js
@@ -0,0 +1,12 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const calendarEl = document.getElementById('calendar');
+const calendar = new Calendar(calendarEl, {
+  plugins: [ dayGridPlugin, interactionPlugin ],
+  initialView: 'dayGridMonth'
+});
+calendar.render();
+
+export default calendar;
