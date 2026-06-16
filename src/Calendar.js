051efe00c77086f6b221d12dfe70dbda1258121e
Darren Whatford <darrenwhatford@Darrens-MacBook-Air.local>
2026-06-16 19:00:17 +0100

    added calender css file

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,6 +1,11 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 const calendarEl = document.getElementById('calendar');
 const calendar = new Calendar(calendarEl, {
@@ -8,5 +13,21 @@ const calendar = new Calendar(calendarEl, {
   initialView: 'dayGridMonth'
 });
 calendar.render();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUye5v1njZsot5vYJHkuhdiK78rxpIAFI",
+  authDomain: "beachmoutain-21b24.firebaseapp.com",
+  projectId: "beachmoutain-21b24",
+  storageBucket: "beachmoutain-21b24.firebasestorage.app",
+  messagingSenderId: "409559234130",
+  appId: "1:409559234130:web:6b792e9301ff5a6f5fd18b",
+  measurementId: "G-4NCCKN4YKZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default Calendar;
